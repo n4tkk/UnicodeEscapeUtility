994925ef2dc8629323b77d01433ee739531925e1
natsuki_k <natsuki@kirigakure.net>
2021-10-06 11:43:56 +0900

    Modify: module body

--- UnicodeEscapeUtility.psm1
+++ UnicodeEscapeUtility.psm1
@@ -5,20 +5,31 @@
 ##
 ################################
 
-function Unicode-UnEscape{
+
+function Unicode-UnEscape
+{
     param
     (
         [string]$InFile,
         [String]$OutFile
     )
     $text = Get-Content -Raw $InFile
+    [Regex]::Unescape($text) | Set-Content -Path $OutFile -Encoding UTF8
 }
 
-function Unicode-Escape{
+function Unicode-Escape
+{
     param
     (
         [string]$InFile,
         [String]$OutFile
     )
     $text = Get-Content -Raw $InFile
+    $callback = {
+        param($m)
+        "\u" + '{0:x}' -f [Convert]::ToInt32([Convert]::ToChar($m.Value))
+    }
+    [Regex]::Replace($text, "[^\x00-\x7F]", $callback) | Set-Content -Path $OutFile -Encoding UTF8
+
 }
+
